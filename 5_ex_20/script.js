ed004cb82485e2b8e5cedce476e5d7920118c887
Fabricio Santos <fabricioliveira0808@gmail.com>
2026-09-10 06:24:37 -0300

    commit ex

---
+++ 5_ex_20/script.js
@@ -0,0 +1,16 @@
+function podeEntrarNaAuto(idade) {
+
+    if(idade >= 18) {
+
+        console.log("Pode se matricular na auto escola");
+
+    } 
+    else {
+        console.log("Não pode se matricular na auto escola");
+    }
+
+}
+
+podeEntrarNaAuto(16);
+podeEntrarNaAuto(18);
+podeEntrarNaAuto(25);
